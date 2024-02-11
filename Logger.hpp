a0fe6069d525e2e258673b4294012ead557559b3
NIR3X <90041219+NIR3X@users.noreply.github.com>
2024-02-11 23:11:03 +0000

    Update Logger.hpp

--- Logger.hpp
+++ Logger.hpp
@@ -5,11 +5,11 @@
 #include <utility>
 
 #define LoggerFprintln_(stream, prefix, file, line) \
-	time_t t = std::time(nullptr); \
-	std::tm tm = {}; \
+	time_t t = time(nullptr); \
+	struct tm tm; \
 	localtime_s(&tm, &t); \
-	char timestamp[20] = {}; \
-	std::strftime(timestamp, sizeof(timestamp), "%Y/%m/%d %H:%M:%S", &tm); \
+	char timestamp[20]; \
+	strftime(timestamp, sizeof(timestamp), "%Y/%m/%d %H:%M:%S", &tm); \
 	stream << timestamp << " " << file << ":" << line << ": " << prefix; \
 	([] <typename... Args> (Args&&... args) { \
 		const char* separator = ""; \
